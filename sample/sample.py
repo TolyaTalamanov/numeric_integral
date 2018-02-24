17c499ea238200b5ad2bee9aa016100f7d11ce15
tolik <talamtolik@gmail.com>
2018-02-24 21:27:00 +0300

    add gaps

--- sample/sample.py
+++ sample/sample.py
@@ -9,19 +9,19 @@ limits   = list(map(int, (input("Enter limits : ").split(' '))))
 integral = integrate.Integrate(sinx)
 
 
-print("exact meaning     = {0}".format(integrate_sin(limits)))
+print("exact meaning     = {0}\n".format(integrate_sin(limits)))
 
-print("integrate trapeze = {0}\nglobal error      = {1}".format(integral.trapeze(limits),
+print("integrate trapeze = {0}\nglobal error      = {1}\n".format(integral.trapeze(limits),
                              abs(integral.trapeze(limits) - integrate_sin(limits))))
 
-print("integrate squad   = {0}\nglobal error      = {1}".format(integral.squad(limits),
+print("integrate squad   = {0}\nglobal error      = {1}\n".format(integral.squad(limits),
                              abs(integral.squad(limits) - integrate_sin(limits))))
 
-print("integrate simpson = {0}\nglobal error      = {1}".format(integral.simpson(limits),
+print("integrate simpson = {0}\nglobal error      = {1}\n".format(integral.simpson(limits),
                              abs(integral.simpson(limits) - integrate_sin(limits))))
 
-print("integrate gauss   = {0}\nglobal error      = {1}".format(integral.gauss(limits),
+print("integrate gauss   = {0}\nglobal error      = {1}\n".format(integral.gauss(limits),
                              abs(integral.gauss(limits) - integrate_sin(limits))))
 
-print("integrate montecarlo   = {0}\nglobal error      = {1}".format(integral.montecarlo(limits),
+print("integrate montecarlo = {0}\nglobal error  = {1}\n".format(integral.montecarlo(limits),
                              abs(integral.montecarlo(limits) - integrate_sin(limits))))
