147c5201df952bc7d722e6c705bee61d03898e4c
tolik <talamtolik@gmail.com>
2018-02-25 13:38:15 +0300

    trasnslate to russian

--- sample/sample.py
+++ sample/sample.py
@@ -5,23 +5,23 @@ def integrate_sin(limits):
     return -cos(limits[1]) + cos(limits[0])
 
 sinx     = lambda x: sin(x)
-limits   = list(map(int, (input("Enter limits : ").split(' '))))
+limits   = list(map(int, (input("Введите пределы интегрирования через пробел : ").split(' '))))
 integral = integrate.Integrate(sinx)
 
 
-print("exact meaning     = {0}\n".format(integrate_sin(limits)))
+print("Точное значение      = {0}\n".format(integrate_sin(limits)))
 
-print("integrate trapeze = {0}\nglobal error      = {1}\n".format(integral.trapeze(limits),
+print("Метод трапеций = {0}\nГлобальная погрешность      = {1}\n".format(integral.trapeze(limits),
                              abs(integral.trapeze(limits) - integrate_sin(limits))))
 
-print("integrate squad   = {0}\nglobal error      = {1}\n".format(integral.squad(limits),
+print("Метод центральных прямоугольников    = {0}\nГлобальная погрешность      = {1}\n".format(integral.squad(limits),
                              abs(integral.squad(limits) - integrate_sin(limits))))
 
-print("integrate simpson = {0}\nglobal error      = {1}\n".format(integral.simpson(limits),
+print("Метод Симпсона = {0}\nГлобальная погрешность      = {1}\n".format(integral.simpson(limits),
                              abs(integral.simpson(limits) - integrate_sin(limits))))
 
-print("integrate gauss   = {0}\nglobal error      = {1}\n".format(integral.gauss(limits),
+print("Метода Гаусса   = {0}\nГлобальная погрешность      = {1}\n".format(integral.gauss(limits),
                              abs(integral.gauss(limits) - integrate_sin(limits))))
 
-print("integrate montecarlo = {0}\nglobal error  = {1}\n".format(integral.montecarlo(limits),
+print("Метод Монте-Карло = {0}\nГлобальная погрешность  = {1}\n".format(integral.montecarlo(limits),
                              abs(integral.montecarlo(limits) - integrate_sin(limits))))
